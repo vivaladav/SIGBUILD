3e0413d7813d831e49fb00505616bddd91f35d31
Davide Coppola <vivaladav@gmail.com>
2019-02-10 17:53:56 +0100

    WIP, close to first working version of table.

--- src/DialogSessionBuilds.cpp
+++ src/DialogSessionBuilds.cpp
@@ -49,31 +49,44 @@ DialogSessionBuilds::DialogSessionBuilds(const QVector<BuildData *> & data, cons
 	QFont font;
 	font.setBold(true);
 
+	const int WIDGET_SPACING = 0;
+
+	const int MARGIN_W = 20;
+
+	//const int STRETCH[NUM_TAB_COLUMNS] = { 30, 25, 25, 15, 5 };
+	const int STRETCH[NUM_TAB_COLUMNS] = { 0, 0, 0, 0, 0 };
+
 	mLayoutHeader = new QHBoxLayout;
-	mLayoutHeader->setContentsMargins(0, 3, 0, 3);
+	mLayoutHeader->setContentsMargins(0, 0, 0, 0);
+	mLayoutHeader->setSpacing(WIDGET_SPACING);
 	header->setLayout(mLayoutHeader);
 
 	QLabel * headerLabels[NUM_TAB_COLUMNS];
 
 	headerLabels[COL_PROJECT] = new QLabel(tr("PROJECT"));
 	headerLabels[COL_PROJECT]->setFont(font);
-	mLayoutHeader->addWidget(headerLabels[COL_PROJECT]);
+	headerLabels[COL_PROJECT]->setContentsMargins(0, 0, MARGIN_W, 0);
+	mLayoutHeader->addWidget(headerLabels[COL_PROJECT], STRETCH[COL_PROJECT]);
 
 	headerLabels[COL_START] = new QLabel(tr("BUILD START"));
 	headerLabels[COL_START]->setFont(font);
-	mLayoutHeader->addWidget(headerLabels[COL_START]);
+	headerLabels[COL_START]->setContentsMargins(MARGIN_W, 0, MARGIN_W, 0);
+	mLayoutHeader->addWidget(headerLabels[COL_START], STRETCH[COL_START]);
 
 	headerLabels[COL_END] = new QLabel(tr("BUILD FINISH"));
 	headerLabels[COL_END]->setFont(font);
-	mLayoutHeader->addWidget(headerLabels[COL_END]);
+	headerLabels[COL_END]->setContentsMargins(MARGIN_W, 0, MARGIN_W, 0);
+	mLayoutHeader->addWidget(headerLabels[COL_END], STRETCH[COL_END]);
 
 	headerLabels[COL_TIME] = new QLabel(tr("BUILD TIME"));
 	headerLabels[COL_TIME]->setFont(font);
-	mLayoutHeader->addWidget(headerLabels[COL_TIME]);
+	headerLabels[COL_TIME]->setContentsMargins(MARGIN_W, 0, MARGIN_W, 0);
+	mLayoutHeader->addWidget(headerLabels[COL_TIME], STRETCH[COL_TIME]);
 
 	headerLabels[COL_RESULT] = new QLabel(tr("RESULT"));
 	headerLabels[COL_RESULT]->setFont(font);
-	mLayoutHeader->addWidget(headerLabels[COL_RESULT]);
+	headerLabels[COL_RESULT]->setContentsMargins(MARGIN_W, 0, 0, 0);
+	mLayoutHeader->addWidget(headerLabels[COL_RESULT], STRETCH[COL_RESULT]);
 
 	// SCROLL AREA
 	QScrollArea * area = new QScrollArea(this);
@@ -87,9 +100,6 @@ DialogSessionBuilds::DialogSessionBuilds(const QVector<BuildData *> & data, cons
 	qDebug() << "QScrollArea sizeAdjustPolicy:" << area->sizeAdjustPolicy();
 	qDebug() << "QScrollArea sizePolicy:" << area->sizePolicy();
 
-//	pal.setColor(QPalette::Background, Qt::blue);
-//	area->setPalette(pal);
-
 	QWidget * scrollContent = new QWidget;
 	scrollContent->setContentsMargins(0, 0, 0, 0);
 	//layout->addWidget(scrollContent);
@@ -97,15 +107,15 @@ DialogSessionBuilds::DialogSessionBuilds(const QVector<BuildData *> & data, cons
 	area->setWidget(scrollContent);
 	//QVBoxLayout * layoutArea = new QVBoxLayout;
 	mLayoutArea = new QVBoxLayout;
-	mLayoutArea->setSpacing(0);
+	mLayoutArea->setSpacing(WIDGET_SPACING);
 	mLayoutArea->setContentsMargins(0, 0, 0, 0);
 	scrollContent->setLayout(mLayoutArea);
 
-//	layoutArea->setColumnStretch(COL_PROJECT, 30);
-//	layoutArea->setColumnStretch(COL_START, 25);
-//	layoutArea->setColumnStretch(COL_END, 25);
+//	layoutArea->setColumnStretch(COL_PROJECT, 35);
+//	layoutArea->setColumnStretch(COL_START, 20);
+//	layoutArea->setColumnStretch(COL_END, 20);
 //	layoutArea->setColumnStretch(COL_TIME, 15);
-//	layoutArea->setColumnStretch(COL_RESULT, 5);
+//	layoutArea->setColumnStretch(COL_RESULT, 10);
 
 	qDebug() << "scrollContent sizePolicy:" << scrollContent->sizePolicy();
 
@@ -121,78 +131,40 @@ DialogSessionBuilds::DialogSessionBuilds(const QVector<BuildData *> & data, cons
 
 		QLabel * label;
 
-		if(i == 3)
+		// PROJECT
+		if(i == 4)
 			label = new QLabel(entry->GetProject() + QString(" an even longer long name for a project..."));
 		else if(i % 2)
 			label = new QLabel(entry->GetProject() + QString(" a very long name for a project..."));
 		else
 			label = new QLabel(entry->GetProject());
 
-		layoutRow->addWidget(label);
-		label->setContentsMargins(0, 0, 0, 0);
+		label->setContentsMargins(0, 0, MARGIN_W, 0);
+		layoutRow->addWidget(label, STRETCH[COL_PROJECT]);
 
-//		connect(label, &Label::SizeChanged, headerLabels[COL_PROJECT], [=](const QSize & newSize)
-//		{
-//			qDebug() << "resize happened...";
-//			qDebug() << "OLD headerLabels[COL_PROJECT]->width():" << headerLabels[COL_PROJECT]->width();
-//			qDebug() << "newSize.width():" << newSize.width();
-
-//			if(newSize.width() > headerLabels[COL_PROJECT]->width())
-//			{
-//				headerLabels[COL_PROJECT]->resize(newSize);
-
-//				mLayoutHeader->update();
-//			}
-
-//			qDebug() << "NEW headerLabels[COL_PROJECT]->width():" << headerLabels[COL_PROJECT]->width();
-
-//			qDebug() << "";
-//		});
-
+		// START
 		label = new QLabel(QDateTime::fromMSecsSinceEpoch(entry->GetTimeStart()).toString("dd-MM-yyyy HH:mm:ss"));
-		label->setContentsMargins(0, 0, 0, 0);
-		layoutRow->addWidget(label);
+		label->setContentsMargins(MARGIN_W, 0, MARGIN_W, 0);
+		layoutRow->addWidget(label, STRETCH[COL_START]);
 
-//		connect(label, &Label::SizeChanged, headerLabels[COL_START], [=](const QSize & newSize)
-//		{
-//			if(newSize.width() > headerLabels[COL_START]->width())
-//				headerLabels[COL_START]->resize(newSize);
-//		});
-
+		// END
 		label = new QLabel(QDateTime::fromMSecsSinceEpoch(entry->GetTimeEnd()).toString("dd-MM-yyyy HH:mm:ss"));
-		label->setContentsMargins(0, 0, 0, 0);
-		layoutRow->addWidget(label);
+		label->setContentsMargins(MARGIN_W, 0, MARGIN_W, 0);
+		layoutRow->addWidget(label, STRETCH[COL_END]);
 
-//		connect(label, &Label::SizeChanged, headerLabels[COL_END], [=](const QSize & newSize)
-//		{
-//			if(newSize.width() > headerLabels[COL_END]->width())
-//				headerLabels[COL_END]->resize(newSize);
-//		});
-
+		// TIME
 		qint64 diff = entry->GetTimeEnd() - entry->GetTimeStart();
 		QTime buildTime(0, 0, 0, 0);
 		buildTime = buildTime.addMSecs(diff);
 		label = new QLabel(buildTime.toString("hh:mm:ss"));
-		label->setContentsMargins(0, 0, 0, 0);
+		label->setContentsMargins(MARGIN_W, 0, MARGIN_W, 0);
+		layoutRow->addWidget(label, STRETCH[COL_TIME]);
 
-		layoutRow->addWidget(label);
-
-//		connect(label, &Label::SizeChanged, headerLabels[COL_TIME], [=](const QSize & newSize)
-//		{
-//			if(newSize.width() > headerLabels[COL_TIME]->width())
-//				headerLabels[COL_TIME]->resize(newSize);
-//		});
-
+		// RESULT
 		label = new QLabel;
-		label->setContentsMargins(0, 0, 0, 0);
+		label->setContentsMargins(MARGIN_W, 0, 0, 0);
 		label->setPixmap(icons[static_cast<int>(entry->GetState())]);
-		layoutRow->addWidget(label);
-
-//		connect(label, &Label::SizeChanged, headerLabels[COL_RESULT], [=](const QSize & newSize)
-//		{
-//			if(newSize.width() > headerLabels[COL_TIME]->width())
-//				headerLabels[COL_RESULT]->resize(newSize);
-//		});
+		layoutRow->addWidget(label, STRETCH[COL_RESULT]);
 	}
 
 	qDebug() << "";
@@ -219,64 +191,66 @@ void DialogSessionBuilds::showEvent(QShowEvent *)
 
 void DialogSessionBuilds::UpdateSizes()
 {
+	qDebug() << "--------------------- 1ST PASS ---------------------";
+
+	const int ROWS = mLayoutArea->count();
+
 	for(int c = 0; c <NUM_TAB_COLUMNS; ++c)
 	{
-		int maxW = mLayoutHeader->itemAt(c)->widget()->minimumSizeHint().width();
-		qDebug() << "H" << c << "=" << maxW;
-
-		const int ROWS = mLayoutArea->count();
+		int maxW = mLayoutHeader->itemAt(c)->widget()->width();
+		int maxWmsh = mLayoutHeader->itemAt(c)->widget()->minimumSizeHint().width();
+		int maxWmw = mLayoutHeader->itemAt(c)->widget()->minimumWidth();
+		qDebug() << "H" << c << "w=" << maxW << "msh w=" << maxWmsh << "mw w=" << maxWmw;
 
 		for(int r = 0; r < ROWS; ++r)
 		{
-			const int W = mLayoutArea->itemAt(r)->layout()->itemAt(c)->widget()->minimumSizeHint().width();
+			const int DW = mLayoutArea->itemAt(r)->layout()->itemAt(c)->widget()->width();
+			const int DWmsh = mLayoutArea->itemAt(r)->layout()->itemAt(c)->widget()->minimumSizeHint().width();
+			const int DWmw = mLayoutArea->itemAt(r)->layout()->itemAt(c)->widget()->minimumWidth();
 
-			qDebug() << "D" << r << "=" << W;
+			qDebug() << "D" << r << "w=" << DW << "msh w=" << DWmsh << "mw w=" << DWmw;
 
-			if(W > maxW)
-				maxW = W;
+			if(DWmsh > maxWmsh)
+				maxWmsh = DWmsh;
 		}
 
-		qDebug() << c << "]" << maxW;
-		qDebug() << "";
+		qDebug() << c << "]" << maxWmsh;
 
-		mLayoutHeader->itemAt(c)->widget()->setMinimumWidth(maxW);
+		mLayoutHeader->itemAt(c)->widget()->setMinimumWidth(maxWmsh);
 
 		for(int r = 0; r < ROWS; ++r)
-			mLayoutArea->itemAt(r)->layout()->itemAt(c)->widget()->setMinimumWidth(maxW);
+			mLayoutArea->itemAt(r)->layout()->itemAt(c)->widget()->setMinimumWidth(maxWmsh);
 	}
 
 	update();
+	mLayoutHeader->update();
+	mLayoutArea->update();
 
-	qDebug() << "---------------------";
-/*
-	for(int c = 0; c < mLayoutHeader->count(); ++c)
+	qDebug() << "--------------------- 2ND PASS ---------------------";
+
+	for(int c = 0; c <NUM_TAB_COLUMNS; ++c)
 	{
-		int maxW = mLayoutHeader->itemAtPosition(0, c)->widget()->width();
-		int maxW = 0;
-		int maxW2 = 0;
-		qDebug() << c << "] maxW H" << maxW;
+		int maxW = mLayoutHeader->itemAt(c)->widget()->width();
+		int maxWmsh = mLayoutHeader->itemAt(c)->widget()->minimumSizeHint().width();
+		int maxWmw = mLayoutHeader->itemAt(c)->widget()->minimumWidth();
+		qDebug() << "H" << c << "w=" << maxW << "msh w=" << maxWmsh << "mw w=" << maxWmw;
 
-		for(int r = 0; r <mLayoutArea->rowCount(); ++r)
+		for(int r = 0; r < ROWS; ++r)
 		{
-			const int W = mLayoutArea->itemAtPosition(r, c)->widget()->width();
+			const int DW = mLayoutArea->itemAt(r)->layout()->itemAt(c)->widget()->width();
+			const int DWmsh = mLayoutArea->itemAt(r)->layout()->itemAt(c)->widget()->minimumSizeHint().width();
+			const int DWmw = mLayoutArea->itemAt(r)->layout()->itemAt(c)->widget()->minimumWidth();
 
-			if(W > maxW)
-				maxW = W;
+			qDebug() << "D" << r << "w=" << DW << "msh w=" << DWmsh << "mw w=" << DWmw;
 
-			const int W2 = mLayoutArea->columnMinimumWidth(c);
-
-			if(W2 > maxW2)
-				maxW2 = W2;
+			if(DWmsh > maxWmsh)
+				maxWmsh = DWmsh;
 		}
 
-		qDebug() << c << "] maxW D" << maxW;
-		qDebug() << c << "] maxW D2" << maxW2;
-		qDebug() << "";
-
-//		mLayoutHeader->setColumnMinimumWidth(c, maxW);
-//		mLayoutArea->setColumnMinimumWidth(c, maxW);
+		qDebug() << c << "]" << maxWmsh;
 	}
-	*/
+
+	qDebug() << "----------------------------------------------------";
 }
 
 } // namespace Sigbuild
